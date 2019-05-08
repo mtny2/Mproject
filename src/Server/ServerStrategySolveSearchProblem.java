c91c1c3c3261426ee6c886b54f7adc8c098d3360
almogsa <almogsa@post.bgu.ac.il>
2019-05-08 10:29:54 +0300

    ya zain

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -3,10 +3,13 @@ package Server;
 //import java.io.InputStream;
 //import java.io.OutputStream;
 
+import IO.MyCompressorOutputStream;
+import IO.MyDecompressorInputStream;
 import algorithms.mazeGenerators.Maze;
 import algorithms.search.*;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy {
@@ -19,27 +22,51 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy {
             ObjectInputStream fromClient = new ObjectInputStream(inputStream);
             ObjectOutputStream toClient = new ObjectOutputStream(outputStream);
             toClient.flush();
-            Solution sol;
+            Solution sol = null;
             // read maze from the client, and create a temp dir
             Maze mazeToClient = (Maze) fromClient.readObject();
             String dir = System.getProperty("java.io.tmpdir");
             String mazeName = mazeToClient.toString();
             byte tempByteArray[] = mazeToClient.toByteArray();
-            //String firstBinaryLine = "";
-            // take the i`s indexes  of the maze and had them to the name
-            //for (int i = 8; i < 100; i++)
-              //  if (i >= tempByteArray.length)
-                //    break;
-                //else firstBinaryLine += tempByteArray[i];
-            //mazeName = mazeName + "-" + firstBinaryLine;
+            String hashName = "mazeSol-" + mazeName.substring(31);
+
+            File SolFileCreate = new File(dir, hashName);
+
             //create file-maze name, and if exist take the solve else solve
             File mazeFileCreate = new File(dir, mazeName);
-            if (mazeFileCreate.exists()) {
-                FileInputStream fileInput = new FileInputStream(mazeFileCreate);
-                ObjectInputStream FileToReturn = new ObjectInputStream(fileInput);
-                sol = (Solution) FileToReturn.readObject();
-                FileToReturn.close();
-            } else {
+
+            File dirTemp = new File(dir);
+            File[] list = dirTemp.listFiles();
+            boolean areMazesEquals = false;
+            if (list != null) {
+                //if(!areMazesEquals)
+                for (File filMaze : list) {
+                    if (filMaze.getName().contains("Maze")) {
+                        byte savedMazeBytes[];
+                        // try {
+                        //read maze from file
+                        InputStream in = new MyDecompressorInputStream(new FileInputStream(filMaze));
+                        //  byte m[] =maze.toByteArray();
+                        // int x=m.length;
+                        savedMazeBytes = new byte[tempByteArray.length];
+                        in.read(savedMazeBytes);
+                        in.close();
+                        areMazesEquals = Arrays.equals(savedMazeBytes, tempByteArray);
+                        if (areMazesEquals) {
+                            for (File filSol : list) {
+                                if (filSol.getName().equals("mazeSol-"+filMaze.getName().substring(31))) {
+                                    FileInputStream fileInput = new FileInputStream(SolFileCreate);
+                                    ObjectInputStream FileToReturn = new ObjectInputStream(fileInput);
+                                    sol = (Solution) FileToReturn.readObject();
+                                    FileToReturn.close();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } if(!areMazesEquals || list==null) {
                 String algSearch;
                 SearchableMaze searchableMaze = new SearchableMaze(mazeToClient); // create a new searchable maze
                 ASearchingAlgorithm algorithmSolve;
@@ -61,11 +88,16 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy {
                     algorithmSolve = new BreadthFirstSearch();
                 sol = algorithmSolve.solve(searchableMaze);
                 //Create "maze" file in the folder.
-                FileOutputStream fileOut = new FileOutputStream(mazeFileCreate);
+                FileOutputStream fileOut = new FileOutputStream(SolFileCreate);
                 ObjectOutputStream objectReturn = new ObjectOutputStream(fileOut);
-
                 objectReturn.writeObject(sol);
                 objectReturn.flush();
+
+                OutputStream out = new MyCompressorOutputStream(new FileOutputStream(mazeFileCreate));
+                out.write(tempByteArray);
+                out.flush();
+                out.close();
+
 
             }
             toClient.writeObject(sol);
